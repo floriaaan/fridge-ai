ee5d35edb3456cac93711f1aaeee8c1551d69ea5
Florian Leroux <florian.leroux3@laposte.net>
2026-08-30 00:26:45 +0200

    feat(mobile): wrap root layout in GestureHandlerRootView

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Slot } from 'expo-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { ThemeProvider } from '../presentation/shared/theme-provider.js'
 import { ConnectorProvider } from '../application/shared/connector-context.js'
 import { createConnector } from '../../providers/create-connector.js'
@@ -10,12 +11,14 @@ export default function RootLayout() {
   const [connector] = useState(() => createConnector())
 
   return (
-    <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <ConnectorProvider connector={connector}>
-          <Slot />
-        </ConnectorProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ConnectorProvider connector={connector}>
+            <Slot />
+          </ConnectorProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   )
 }
